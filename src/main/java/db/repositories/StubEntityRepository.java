521e7b4be9b37b8ff6c449d936ebecebe57661cc
Marsuz <marcin.zajda@gmail.com>
2016-08-20 16:37:23 +0200

    Initial db connection works

---
+++ src/main/java/db/repositories/StubEntityRepository.java
@@ -0,0 +1,33 @@
+package db.repositories;
+
+import model.test.StubEntity;
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static db.utils.HibernateUtils.*;
+
+/**
+ * Created by Marcin on 2016-08-20.
+ */
+@Repository
+public class StubEntityRepository {
+
+    public void saveEntites(List<StubEntity> entities) {
+        Session session = getSession();
+        session.beginTransaction();
+        for(StubEntity entity : entities) {
+            session.save(entity);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public List<StubEntity> getAllEntities() {
+        Session session = getSession();
+        List<StubEntity> result = session.createCriteria(StubEntity.class).list();
+        session.close();
+        return result;
+    }
+}
---
+++ src/main/java/rest/BaseController.java
@@ -0,0 +1,41 @@
+package rest;
+
+import db.repositories.StubEntityRepository;
+import model.test.StubEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ScannedGenericBeanDefinition;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Marcin on 2016-08-20.
+ */
+
+@RestController
+public class BaseController {
+
+    @Autowired
+    StubEntityRepository stubEntityRepository;
+
+    @RequestMapping(value="/")
+    public List<StubEntity> getAllEntities() {
+        return stubEntityRepository.getAllEntities();
+    }
+
+    @RequestMapping(value="/testSave")
+    public void generateDummyData() {
+        StubEntity stub1 = new StubEntity("wojtek");
+        StubEntity stub2 = new StubEntity("sara");
+        StubEntity stub3 = new StubEntity("mateusz");
+        StubEntity stub4 = new StubEntity("marcin");
+        List<StubEntity> toSave = new ArrayList<StubEntity>();
+        toSave.add(stub1);
+        toSave.add(stub2);
+        toSave.add(stub3);
+        toSave.add(stub4);
+        stubEntityRepository.saveEntites(toSave);
+    }
+}
